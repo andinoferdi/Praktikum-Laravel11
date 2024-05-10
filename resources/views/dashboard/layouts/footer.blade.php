a3263d01d7481cbae8155da5e4b4e5a7bf6bd564
Ferdi <andinoferdiansah@gmail.com>
2024-05-11 00:34:02 +0700

    Update

---
+++ resources/views/dashboard/layouts/footer.blade.php
@@ -0,0 +1,19 @@
+ <footer class="content-footer footer bg-footer-theme">
+     <div class="container-xxl d-flex flex-wrap justify-content-center py-2 flex-md-row flex-column">
+         <div class="mb-2 mb-md-0 text-center">
+             ©
+             <script>
+                 document.write(new Date().getFullYear());
+             </script>
+             © Copyright <span id="displayYear"></span>
+             © <span id="displayYear"></span> Created By
+             <a>Andino Ferdiansah</a>
+             Template By
+             <a href="https://themewagon.com/" target="_blank" class="footer-link fw-bolder">ThemeWagon</a>
+             <a href="https://www.youtube.com/channel/UCi4AAITeZf7Txpws4eK-uQw" target="_blank"><i
+                     class="bx bxl-youtube"></i></a>
+             <a href="https://instagram.com/andinoferdi" target="_blank"><i class="bx bxl-instagram"></i></a>
+         </div>
+     </div>
+
+ </footer>
